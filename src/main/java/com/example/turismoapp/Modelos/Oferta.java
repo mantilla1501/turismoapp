ea9411fc2103c6083f18824a223ea13173bd18f9
desarrollo <ardavid9801@gmail.com>
2023-08-23 12:04:20 -0500

    proyecto nuevo

--- src/main/java/com/example/turismoapp/Modelos/Oferta.java
+++ src/main/java/com/example/turismoapp/Modelos/Oferta.java
@@ -1,14 +1,85 @@
 package com.example.turismoapp.Modelos;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
+
+@Entity
+@Table(name="oferta")
 
 public class Oferta {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Integer id;
+
+    @Column(name = "nombre",nullable  = false )
     private  String titulo;
     private  String descripcion;
     private LocalDate fechaInicial;
     private LocalDate fechaFin;
     private  Double costoPersona;
 
+    public Oferta() {
+    }
+
+    public Oferta(Integer id, String titulo, String descripcion, LocalDate fechaInicial, LocalDate fechaFin, Double costoPersona) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fechaInicial = fechaInicial;
+        this.fechaFin = fechaFin;
+        this.costoPersona = costoPersona;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDate getFechaInicial() {
+        return fechaInicial;
+    }
+
+    public void setFechaInicial(LocalDate fechaInicial) {
+        this.fechaInicial = fechaInicial;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Double getCostoPersona() {
+        return costoPersona;
+    }
+
+    public void setCostoPersona(Double costoPersona) {
+        this.costoPersona = costoPersona;
+    }
 }
